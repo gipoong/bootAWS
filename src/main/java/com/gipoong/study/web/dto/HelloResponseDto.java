c921b27a1b28fdc8989eea14de8e51aec17a5f98
Gipoong <gipoong@naver.com>
2022-08-06 21:28:29 +0900

    - Gradle 5.x 이상 버전의 lombok 의존성 주입  - dto Test

---
+++ src/main/java/com/gipoong/study/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.gipoong.study.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
