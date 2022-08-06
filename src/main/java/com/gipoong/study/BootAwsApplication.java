733613623db026d939e0ca19ba46c898b98817c4
Gipoong <gipoong@naver.com>
2022-08-06 20:39:15 +0900

    Initial commit

---
+++ src/main/java/com/gipoong/study/BootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.gipoong.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gipoong/study/BootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gipoong.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
